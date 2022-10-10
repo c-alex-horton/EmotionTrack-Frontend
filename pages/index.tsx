cdbba9b45715b484afc7806fee619563e8bf6665
Caleb Horton <c.alex.horton@gmail.com>
2022-10-10 09:53:13 -0500

    home styling and added Register page

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,68 +20,70 @@ import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-    <Container maxWidth='container.xl' padding={0} bg='blue.300'>
-      <Head>
-        <title>Emotion Track</title>
-        <meta name='description' content='Emotion Tracking Software' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Flex h='100vh' py={20} align='center' direction='column'>
-        <HStack
-          w='full'
-          h='full'
-          p={1}
-          spacing={10}
-          alignItems='center'
-          justify='center'>
-          <Box>
-            <Image src='/Logo.svg' alt='Emotion Track' />
-          </Box>
-        </HStack>
-        <HStack
-          w='full'
-          h='full'
-          p={10}
-          spacing={10}
-          alignItems='center'
-          justify='center'>
-          <FormControl>
-            <VStack spacing={10}>
-              <Input
-                type='email'
-                placeholder='Email'
-                variant='flushed'
-                color='white'
-                _placeholder={{ color: 'inherit' }}
-                maxWidth='md'
-              />
-              <Input
-                type='password'
-                placeholder='Password'
-                variant='flushed'
-                color='white'
-                _placeholder={{ color: 'inherit' }}
-                maxWidth='md'
-              />
-              <Button
-                rightIcon={<ArrowForwardIcon />}
-                variant='outline'
-                color='white'>
-                Login
-              </Button>
-            </VStack>
-          </FormControl>
-        </HStack>
-        <Text color='white'>
-          Don&apos;t Have an account?{' '}
-          <Link href='#'>
-            <span style={{ textDecoration: 'underline', cursor: 'pointer' }}>
-              Register
-            </span>
-          </Link>
-        </Text>
-      </Flex>
-    </Container>
+    <Box bg='blue.300'>
+      <Container maxWidth='container.xl' padding={0}>
+        <Head>
+          <title>Emotion Track</title>
+          <meta name='description' content='Emotion Tracking Software' />
+          <link rel='icon' href='/favicon.ico' />
+        </Head>
+        <Flex h='100vh' py={20} align='center' direction='column'>
+          <HStack
+            w='full'
+            h='full'
+            p={1}
+            spacing={10}
+            alignItems='center'
+            justify='center'>
+            <Box>
+              <Image src='/Logo.svg' alt='Emotion Track' />
+            </Box>
+          </HStack>
+          <HStack
+            w='full'
+            h='full'
+            p={10}
+            spacing={10}
+            alignItems='center'
+            justify='center'>
+            <FormControl>
+              <VStack spacing={10}>
+                <Input
+                  type='email'
+                  placeholder='Email'
+                  variant='flushed'
+                  color='white'
+                  _placeholder={{ color: 'inherit' }}
+                  maxWidth='md'
+                />
+                <Input
+                  type='password'
+                  placeholder='Password'
+                  variant='flushed'
+                  color='white'
+                  _placeholder={{ color: 'inherit' }}
+                  maxWidth='md'
+                />
+                <Button
+                  rightIcon={<ArrowForwardIcon />}
+                  variant='outline'
+                  color='white'>
+                  Login
+                </Button>
+              </VStack>
+            </FormControl>
+          </HStack>
+          <Text color='white'>
+            Don&apos;t have an account?{' '}
+            <Link href='/register'>
+              <span style={{ textDecoration: 'underline', cursor: 'pointer' }}>
+                Register
+              </span>
+            </Link>
+          </Text>
+        </Flex>
+      </Container>
+    </Box>
   )
 }
 
